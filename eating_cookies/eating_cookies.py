fa6250cae3fc1d4c98b3c5a8950b919b14de02be
Alvin <alvin.frierson@fuller.edu>
2019-05-02 15:13:26 -0700

    completed eating cookies algorithm.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,13 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n, cache=[1, 1, 2]):
+    if not n in range(0, len(cache)):
+        value = eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+        cache.append(value)
+    return cache[n]
+
+#eating_cookies(4)    
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:

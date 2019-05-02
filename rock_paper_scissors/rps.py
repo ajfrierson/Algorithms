30b5168a08f4ff79720545c2dbaf3742fe4088ed
Alvin <alvin.frierson@fuller.edu>
2019-05-02 14:29:55 -0700

    completed rock paper scissors algorithm.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,29 @@
 import sys
 
 
+
 def rock_paper_scissors(n):
- 
+    output = []
+    plays = ['rock', 'paper', 'scissors']
+
+    # using a helper recursion method since need to return a list
+    def helper_method(m, lst):
+        # base case
+        if m == 0:
+            output.append(lst)
+            return
+
+        # looping through plays to insert it into array
+        for play in plays:
+            # making a newlist since don't want to reference old one
+            newlst = lst[:]
+            newlst.append(play)
+            helper_method(m-1, newlst)
+
+    # calling helper method with n and empty arr to work with
+    helper_method(n, [])
+
+    return output
 
 
 if __name__ == "__main__":
